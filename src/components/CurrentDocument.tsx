8df6ee2af2368aeff5a672bd28b94bf774be9dd0
Peter Abah <peterabah.ob@gmail.com>
2022-06-14 16:26:02 +0100

    Add editing of document

--- src/components/CurrentDocument.tsx
+++ src/components/CurrentDocument.tsx
@@ -1,11 +1,26 @@
+import { useState, useEffect, ChangeEvent } from 'react';
 import { useDocuments } from '@/contexts/DocumentsContext';
 import DocumentPreview from './DocumentPreview';
 
 const CurrentDocument = () => {
-  const { currentDoc } = useDocuments();
+  const { currentDoc, updateDoc } = useDocuments();
+  const [content, setContent] = useState(currentDoc?.content || '');
   
+  // Change content when currentDoc changes
+  useEffect(() => {
+   setContent(currentDoc?.content || '');
+ }, [currentDoc]);
+
   if (currentDoc == null) {
     return null;
+  }
+  
+  const updateContent = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.currentTarget.value);
+  };
+  
+  const onSave = () => {
+    updateDoc({ ...currentDoc, content });
   }
 
   return (
@@ -14,8 +29,14 @@ const CurrentDocument = () => {
         <h2>{currentDoc.name}</h2>
         <p>{currentDoc.updated_at.toString()}</p>
       </header>
- 
-      <DocumentPreview content={currentDoc.content} />
+      
+      <textarea
+        value={content}
+        onChange={updateContent}
+      />
+      
+      <button type='button' onClick={onSave}>Save</button>
+      <DocumentPreview content={content} />
     </section>
    )
 };
