c8b3ef71b261874e68fc842e2bf98e16946fa130
Peter Abah <peterabah.ob@gmail.com>
2022-06-14 18:34:32 +0100

    Add styles to menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,13 @@
+import Documents from '../Documents';
+import './index.css';
+
+const Menu = () => {
+  return (
+    <aside className='menu'>
+      <h1 className='menu-title'>MARKDOWN</h1>
+      <Documents />
+    </aside>
+  )
+};
+
+export default Menu;
